bc8e3df38e40bc97d074bf4299ee26552a9618cc
Harman Goei <5001432+hrgui@users.noreply.github.com>
2022-05-21 20:05:03 -0700

    feat: hello, world or name

---
+++ 1-hello-world/README.md
@@ -0,0 +1,11 @@
+# To run a go program
+
+`go run <filename>`
+
+# To test
+
+`go test`
+
+# to watch
+
+- install gow https://github.com/mitranim/gow#installation
---
+++ 1-hello-world/hello_test.go
@@ -0,0 +1,29 @@
+package main
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	// to not repeat got != want
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		t.Helper() // this tells that when a test fails, the failure won't be in here but rather in the caller of this helper
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	}
+
+
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Harman")
+		want := "Hello, Harman"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
+}
